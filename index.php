7074768836657f9c0001c1ba852582c3e7ead349
Valdur Kana itk.khk.ee <valdur55@gmail.com>
2015-09-28 21:31:58 +0300

    Read projects from csv file [Deliver #104273020]

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
 
-$projects = Array(1437300, 1420598, 1425026, 1431288, 1431204);
+//$projects = Array(1437300);
+$projects = Array();
+$file = fopen("VS15 Projektid - Pseudoülesannete projekt.csv","r");
+
+while(! feof($file))
+//FIXME:csv file withot read premission gives endless loop.
+      {
+          $line = fgetcsv($file);
+          if (stristr($line[2], 'pivotaltracker.com/n/projects/', false) ){
+              $p_link = explode("/",$line[2]);
+              $projects[$p_link[5]]=$line[0];
+              //$projects[]=
+          }
+              }
+fclose($file);
+
 class Worker{
     var $PIVO_SERVICE_URL = "/services/v5/projects/";
     var $projects;
+    var $pids;
     var $cdata;
     var $tdata;
+    var $with_error= Array();
     var $row_data = array (
         'name' => "/?fields=name",
         'last_update' => "/activity?limit=1&offset=0&fields=message,occurred_at",
@@ -15,6 +32,7 @@ class Worker{
 
     function Worker($projects){
         $this->projects = $projects;
+        $this->pids = array_keys($projects);
         $this->cdata=$this->get_curl_data();
     }
 
@@ -24,7 +42,7 @@ class Worker{
 
     function make_urls() {
         $pages = array();
-        foreach($this->projects as $pid) {
+        foreach($this->pids as $pid) {
             foreach($this->row_data as $k => $endpoint){
                 array_push($pages, $this->get_url($pid, $endpoint));
             }
@@ -68,9 +86,13 @@ class Worker{
         $row = Array();
         //Projekti nimi
         $data = $this->get_data($pid, 'name');
-        $name = '<a href=https://www.pivotaltracker.com/n/projects/'.$data->id.'>'.$data->name.'</a>';
-        array_push($row, $name);
-
+        if ( array_key_exists('kind', $data)) {
+            $this->with_error[]=$this->projects[$pid];
+            return $row;
+        }else {
+            $name = '<a href=https://www.pivotaltracker.com/n/projects/'.$data->id.'>'.$data->name.'</a>';
+            array_push($row, $name);
+        };
         //Viimase uuenduse pealkiri ja aeg
         $data = $this->get_data($pid, 'last_update');
         array_push($row, $data[0]->message);
@@ -102,14 +124,20 @@ class Worker{
 
     public function get_table_data(){
         $tdata = array();
-        foreach($this->projects as $pid){
+        foreach($this->pids as $pid){
             array_push($tdata,$this->get_row($pid));
         }
         return $tdata;
     }
+
+    public function get_with_error(){
+        return $this->with_error;
+    }
 }
 
-$rows = new worker($projects);
+$pivo_staatus = new worker($projects);
+$status_table = $pivo_staatus->get_table_data();
+$with_error = $pivo_staatus->get_with_error();
 $pealkiri = Array("Projekti nimi", "Viimase  kande pealkiri", "Aeg", "Commiti message", "Aeg", "Acceptimata storyd");
 ?>
 
@@ -121,6 +149,15 @@ $pealkiri = Array("Projekti nimi", "Viimase  kande pealkiri", "Aeg", "Commiti me
 </head>
 <body>
 
+<?php if($with_error): ?>
+    <h2>Järgmiste isikute Pivotal Trackeri projekti lugemisega tekkis probleem:</h2>
+    <ol>
+        <?php foreach ($with_error as $el): ?>
+            <li><?= $el ?></li>
+        <? endforeach ?>
+    </ol>
+<?php endif ?>
+
 <table border=1>
 
     <thead>
@@ -131,7 +168,7 @@ $pealkiri = Array("Projekti nimi", "Viimase  kande pealkiri", "Aeg", "Commiti me
 
     </thead>
 
-    <?php foreach ($rows->get_table_data() as $row): ?>
+    <?php foreach ($status_table as $row): ?>
         <tr>
             <? foreach ($row as &$column): ?>
 
